842cf0559d5a5d2a3a1db25bafe33c8f942f449a
xuzxiang97 <1090765806@qq.com>
2019-10-18 18:23:51 +0800

    Initial commit

---
+++ src/main/java/com/laofuzi/book/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.laofuzi.book.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+    @RequestMapping("/index")
+    @ResponseBody
+    public String test(){
+        return "主页";
+    }
+}
