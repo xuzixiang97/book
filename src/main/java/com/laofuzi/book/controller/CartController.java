c6232236beeeae4cea419f73c3850db82ef573e8
xuzxiang97 <1090765806@qq.com>
2019-12-01 15:30:41 +0800

    Controller

---
+++ src/main/java/com/laofuzi/book/controller/CartController.java
@@ -0,0 +1,73 @@
+package com.laofuzi.book.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.laofuzi.book.entity.Cart;
+import com.laofuzi.book.entity.Order;
+import com.laofuzi.book.entity.Result;
+import com.laofuzi.book.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(path = "/cart")
+public class CartController {
+
+    @Autowired
+    private CartService cartService;
+
+    /**
+     * 新增购物车
+     * @param cart
+     * @return
+     */
+    @RequestMapping(path = "/add", method = RequestMethod.POST)
+    @ResponseBody
+    public String add(@RequestBody Cart cart){
+        Cart insert = cartService.insert(cart);
+        return JSON.toJSONString(insert);
+    }
+
+    /**
+     * 修改购物车信息
+     * @param cart
+     * @return
+     */
+    @RequestMapping(path = "/update", method = RequestMethod.POST)
+    @ResponseBody
+    public String update(@RequestBody Cart cart){
+        int i = cartService.updateById(cart);
+        return JSON.toJSONString(new Result(i>0));
+    }
+
+    /**
+     * 删除购物车
+     * @param cart
+     * @return
+     */
+    @RequestMapping(path = "/delete", method = RequestMethod.POST)
+    @ResponseBody
+    public String delete(@RequestBody Cart cart){
+        int i = cartService.deleteById(cart.getId());
+        return JSON.toJSONString(new Result(i>0));
+    }
+
+    /**
+     * 购物车列表列表（全部）
+     * @param cart
+     * @return
+     */
+    @RequestMapping(path = "/findAll", method = RequestMethod.POST)
+    @ResponseBody
+    public String findAll(@RequestBody Cart cart){
+        List<Cart> list = cartService.selectAll();
+        return JSON.toJSONString(list);
+    }
+
+}
+
---
+++ src/main/java/com/laofuzi/book/controller/OrderItemsController.java
@@ -0,0 +1,73 @@
+package com.laofuzi.book.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.laofuzi.book.entity.Order;
+import com.laofuzi.book.entity.OrderItems;
+import com.laofuzi.book.entity.Result;
+import com.laofuzi.book.service.OrderItemsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(path = "/order_items")
+public class OrderItemsController {
+
+    @Autowired
+    private OrderItemsService orderItemsService;
+
+    /**
+     * 新增订单
+     * @param orderItems
+     * @return
+     */
+    @RequestMapping(path = "/add", method = RequestMethod.POST)
+    @ResponseBody
+    public String add(@RequestBody OrderItems orderItems){
+        OrderItems insert = orderItemsService.insert(orderItems);
+        return JSON.toJSONString(insert);
+    }
+
+    /**
+     * 修改订单
+     * @param orderItems
+     * @return
+     */
+    @RequestMapping(path = "/update", method = RequestMethod.POST)
+    @ResponseBody
+    public String update(@RequestBody OrderItems orderItems){
+        int i = orderItemsService.updateById(orderItems);
+        return JSON.toJSONString(new Result(i>0));
+    }
+
+    /**
+     * 删除订单
+     * @param orderItems
+     * @return
+     */
+    @RequestMapping(path = "/delete", method = RequestMethod.POST)
+    @ResponseBody
+    public String delete(@RequestBody OrderItems orderItems){
+        int i = orderItemsService.deleteById(orderItems.getId());
+        return JSON.toJSONString(new Result(i>0));
+    }
+
+    /**
+     * 订单列表（全部）
+     * @param orderItems
+     * @return
+     */
+    @RequestMapping(path = "/findAll", method = RequestMethod.POST)
+    @ResponseBody
+    public String findAll(@RequestBody OrderItems orderItems){
+        List<OrderItems> list = orderItemsService.selectAll();
+        return JSON.toJSONString(list);
+    }
+
+}
+
